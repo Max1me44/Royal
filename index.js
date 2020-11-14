0ca2a1a2ddc08d29c1c97504b5753af3a82ab4ed
CarqueMax <mamaxdu44470@gmail.com>
2020-11-14 04:48:37 +0100

    Correction bugs, v1.1

--- index.js
+++ index.js
@@ -48,10 +48,10 @@ client.on("message", async message => {
             .setDescription("```HELP " + client.user.username + "```")
             .setThumbnail(ppbot)
 	        .addFields(
-	        	{ name: prefix + "helpmod", value: 'Commandes modération', inline: true },
-	        	{ name: prefix + "helpfun", value: 'Commandes fun', inline: true },
-                { name: prefix + "helpother", value: 'Autres commandes', inline: true },
-                { name: prefix + "helpall", value: 'Donne toutes les commandes', inline: true },
+	        	{ name: prefix + "helpmod", value: 'Commandes modération' },
+	        	{ name: prefix + "helpfun", value: 'Commandes fun' },
+                { name: prefix + "helpother", value: 'Autres commandes' },
+                { name: prefix + "helpall", value: 'Donne toutes les commandes' },
 	        )
 	        .setTimestamp()
             .setFooter(`${client.user.tag}`, ppbot)
@@ -80,7 +80,7 @@ client.on("message", async message => {
         .setDescription("```HELP FUN " + client.user.username + "```")
         .setThumbnail(ppbot)
 	    .addFields(
-            { name: prefix + "8ball <question>", value: 'Le bot te répond', inline: true },
+            { name: prefix + "8ball <question>", value: 'Le bot répond à te question'},
 	    )
 	    .setTimestamp()
         .setFooter(`${client.user.tag}`, ppbot)
@@ -90,12 +90,12 @@ client.on("message", async message => {
         .setDescription("```HELP OTHER " + client.user.username + "```")
         .setThumbnail(ppbot)
 	    .addFields(
-	    	{ name: prefix + "invite", value: 'Invite le bot sur tes serveurs', inline: true },
-            { name: prefix + "botstats", value: 'Infos sur le bot', inline: true },
-            { name: prefix + "userstats <membre>", value: 'Infos sur ton compte', inline: true },
-            { name: prefix + "pp <membre>", value: 'Photo de profil discord', inline: true },
-            { name: prefix + "servstats", value: 'Infos sur ce serveur', inline: true },
-            { name: prefix + "ping", value: 'Ping du bot', inline: true },
+	    	{ name: prefix + "invite", value: 'Invite le bot sur tes serveurs' },
+            { name: prefix + "botstats", value: 'Infos sur le bot'},
+            { name: prefix + "userstats <membre>", value: 'Infos sur ton compte' },
+            { name: prefix + "pp <membre>", value: 'Photo de profil discord' },
+            { name: prefix + "servstats", value: 'Infos sur ce serveur' },
+            { name: prefix + "ping", value: 'Ping du bot' },
 	    )
 	    .setTimestamp()
         .setFooter(`${client.user.tag}`, ppbot)
@@ -117,9 +117,9 @@ client.on("message", async message => {
 });
 
 
-// GENERAL COMMANDES
+// OTHER COMMANDES
 client.on("message", async message => {
-    if(message.content.startsWith(prefix + "invite")) {
+    if(message.content === prefix + "invite") {
         var invite_embed = new Discord.MessageEmbed()
             .setTitle("**Invite le bot sur un de tes serveurs**")
             .setDescription("Bot développé par <@398863083176722432> dans le but de s'entrainer en JavaScript. Il possède des commandes de modération :tools: , quelques petits jeux, ainsi que d'autre commandes sympa :bar_chart: .")
@@ -128,11 +128,11 @@ client.on("message", async message => {
         message.channel.send(invite_embed)
     }
 
-    if(message.content.startsWith(prefix + "botstats")) {
+    if(message.content === prefix + "botstats") {
         function conversion_seconde_heure() {
             // "process.uptime()" = fonction node pour avoir le temps en s
             // si reste= 123332221 alors 1427j 10h 57min 1s
-            var reste= process.uptime();
+            var reste= parseInt(process.uptime());
             var result='';
             var nbJours=Math.floor(reste/(3600*24));
             reste -= nbJours*24*3600;
@@ -151,18 +151,32 @@ client.on("message", async message => {
                 result=result+nbSeconds+'s ';
             return result;
         }
+        var plurielServeur;
+        if(client.guilds.cache.size > 1) {
+            plurielServeur = 's';
+        } else {
+            plurielServeur = '';
+        }
+        var plurielUser;
+        if(client.users.cache.size > 1) {
+            plurielUser = 's';
+        } else {
+            plurielUser = '';
+        }
+        var ping = Date.now() - message.createdTimestamp;
         var botstats_embed = new Discord.MessageEmbed()
             .setTitle(`:pencil: Informations sur ${client.user.tag}`)
             .setDescription("--------------------------------------------")
             .addFields(
-                { name: ":satellite: Actif sur :", value: client.guilds.cache.size + " serveur(s)", inline: true },
                 { name: ":clipboard: Nom :", value: client.user.tag, inline: true },
-                { name: ":bar_chart: Utilisateurs :", value: client.users.cache.size + " utilisateur(s)", inline: true },
-                { name: ":desktop: Développeur :", value: "<@398863083176722432>", inline: true },
-                { name: ":ping_pong: Ping du bot :", value: Math.round(client.ping) + " Milliseconds", inline: true },
-                { name: ":symbols: Prefix :", value: "``" + prefix + "``", inline: true },
-                { name: ":beginner: Serveur support :", value: "none", inline: true },
+                { name: ":id: ID :", value: client.user.id, inline: true },
+                { name: ":symbols: Prefix :", value: "``" + prefix + "``" },
+                { name: ":satellite: Actif sur :", value: client.guilds.cache.size + "serveur" + plurielServeur, inline: true },
+                { name: ":bar_chart: Utilisateurs :", value: client.users.cache.size + " utilisateur" + plurielUser, inline: true },
+                { name: ":beginner: Serveur support :", value: "none" },
                 { name: ":battery: En ligne depuis :", value: conversion_seconde_heure(), inline: true },
+                { name: ":ping_pong: Ping du bot :", value: ping + " Milliseconds", inline: true },
+                { name: ":desktop: Développeur :", value: "<@398863083176722432>", inline: true },
             )
             .setThumbnail(ppbot)
             .setTimestamp()
@@ -171,16 +185,18 @@ client.on("message", async message => {
     }
 
     if(message.content.startsWith(prefix + "userstats")) {
-        const membre = message.mentions.users.first() || message.author;
+        const user = message.mentions.users.first() || message.author;
         var userstats_embed = new Discord.MessageEmbed()
             .setAuthor(message.author.username)
             .setTitle("Voici quelques infos sur ce compte")
-            .addField("Nom entier", `${membre.tag}`)
-            .addField("ID", membre.id)
-            .addField("Créé le", membre.createdAt)
-            .addField("Tu as rejoint ce serveur le", membre.joinedAt)
-            //.setThumbnail(membre.avatarURL)
-            .setImage(membre.avatarURL)
+            .setDescription("--------------------------------------------")
+            .addFields(
+                { name: ":clipboard: Nom entier ", value: user.tag, inline: true },
+                { name: ":id: ID : ", value: user.id, inline: true },
+                { name: "Créé le : ", value: user.createdAt },
+                { name: ":inbox_tray: Tu as rejoint ce serveur le : ", value: "undefined", inline: true },
+            )
+            .setImage(user.avatarURL())
             .setTimestamp()
             .setFooter(`${client.user.tag}`, ppbot)
         message.channel.send(userstats_embed);
@@ -191,36 +207,63 @@ client.on("message", async message => {
         var pp_embed = new Discord.MessageEmbed()
             .setColor(0x333333)
             .setAuthor(user.username)
-            .setImage(user.avatarURL);
+            .setImage(user.avatarURL());
         message.channel.send(pp_embed);
     }
 
-    if (message.content.startsWith(prefix + "servstats")) {
+    if(message.content === prefix + "servstats") {
+        const guild = message.guild; // Récupère la guild
+        if(!guild.available) return; // Stop si la guild n'existe pas
+        await message.guild.members.fetch(message.guild.ownerID) // Récupère le proprio
+              .then(guildMember => sOwner = guildMember) // le trouve
+        var propio = guild.member(sOwner) ? sOwner.toString() : guild.owner.user.tag;
+
         var servstats_embed = new Discord.MessageEmbed()
             .setTitle("Voici quelques infos sur ce serveur")
-            .addField('Nom', `${message.guild.name}`, (`${message.guild.nameAcronym, true}`))
-            .addField('ID', message.guild.id)
-            //.addField('Propriétaire du serveur', message.guild.owner.user.tag)
-            .addField("Date de création du serveur", message.guild.createdAt)
+            .setDescription("--------------------------------------------")
+            .addField(':clipboard: Nom', `${message.guild.name}`, (`${message.guild.nameAcronym, true}`, true))
+            .addField(':id: ID', message.guild.id, true)
+            .addField(':crown: Propriétaire du serveur', propio)
+            .addField("Nombre de membre", message.guild.memberCount, true)
+            .addField("Nombre de channels", message.guild.channels.cache.size, true)
+            .addField(":checkered_flag: Date de création du serveur", message.guild.createdAt)
+            .addField("Nombre d'émoji", message.guild.emojis.cache.size, true)
+            .addField("Nombre de rôle", message.guild.roles.cache.size, true)
+            .addField("Nombre de boost", message.guild.premiumSubscriptionCount, true)
             .addField("Localisé en ", message.guild.region)
-            .addField("Nombre de membres", message.guild.memberCount, true)
-            //.addField("Nombre de channels", message.guild.channelsCount, true)
-            .addField("Nombre de boosts", message.guild.premiumSubscriptionCount, true)
-            //.setThumbnail(message.guild.icon)
-            //.setImage(message.guild.icon)
+            .setImage(message.guild.iconURL())
             .setTimestamp()
             .setFooter(`${client.user.tag}`, ppbot)
         message.channel.send(servstats_embed);
     }
 
     if(message.content === prefix + "ping"){
+        var ping = Date.now() - message.createdTimestamp;
         var ping_embed = new Discord.MessageEmbed()
         .setAuthor(`${client.user.tag}`, client.user.avatarURL)
         .setFooter(`${client.user.tag}`, ppbot)
         .setTimestamp()
-        .addField(`Latence actuelle de ${client.user.username} :`, Math.floor(message.createdTimestap - message.createdTimestap) + " Milliseconds")
-        .addField(`Latence de l'API :`, Math.round(client.ping) + " Milliseconds")
+        .addField(`Latence actuelle de ${client.user.username} :`, ping + `Milliseconds`)
+        .addField(`Latence de l'API :`, client.ws.ping + " Milliseconds")
         message.channel.send(ping_embed)
     }
 });
 
+// FUN COMMANDES
+client.on("message", async message => {
+    let messageArray = message.content.split(" ")
+    let args = messageArray.slice(1)
+    if(message.content.startsWith(prefix + "8ball")) {
+        if (!args[0]) return message.channel.send("Il me semble que tu as oublié de poser la question :confused:")
+        let reponse = ["Oui","Non","Bats les couilles, va faire chier un autre bot","Certainement pas, vu la gueule de ce chacal","Mouais vite fait","Malheureusement, c'est sans espoir","A 100% !!!","Bien sur",];
+        let random = Math.floor((Math.random() * reponse.length));
+        let question = args.slice(0).join(" ");
+        let ball_embed = new Discord.MessageEmbed()
+            .setTitle(`**Commandes 8ball de __${message.author.tag}__**`)
+            .addField("``Ta question:``", question)
+            .addField("``Réponse:``", reponse[random])
+            .setFooter(client.user.tag, client.user.avatarURL)
+            .setColor("RANDOM")
+        message.channel.send(ball_embed)
+    }
+});
